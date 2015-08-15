43e53ba7d63cfe8f8c7b14608f73250eb8328c61
wojtek333314 <wojtek33314a@o2.pl>
2015-08-15 15:55:04 +0200

    test3

--- android/src/com/brotherhood/wizards/android/AndroidLauncher.java
+++ android/src/com/brotherhood/wizards/android/AndroidLauncher.java
@@ -12,5 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MainActivity(), config);
+
+		System.out.println("SSS");
 	}
 }

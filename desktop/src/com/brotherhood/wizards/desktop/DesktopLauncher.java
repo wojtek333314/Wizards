573d06776f68a713b9301af2ed6fd12f87c6ee9b
wojtek333314 <wojtek33314a@o2.pl>
2015-08-15 15:42:18 +0200

    firstStep
    firstStep

---
+++ desktop/src/com/brotherhood/wizards/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brotherhood.wizards.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brotherhood.wizards.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
